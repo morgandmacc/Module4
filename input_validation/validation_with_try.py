8759c4507ce5c268c6fd8ed2771405d4adff3595
morganmccarley <morgan.mccarley@gmail.com>
2020-09-15 20:04:02 -0500

    changing from 3 scores to 4 scores

--- input_validation/validation_with_try.py
+++ input_validation/validation_with_try.py
@@ -5,10 +5,10 @@ Date: 9/15/2020
 
 program prompts for three test scores and returns the average to the user.
 """
-NUMBER_TESTS = 3
+NUMBER_TESTS = 4
 
 
-def average(score1, score2, score3):
+def average(score1, score2, score3, score4):
     if score1 < 0 or score2 < 0 or score3 < 0:
         raise ValueError
     return float((score1 + score2 + score3)/NUMBER_TESTS)
@@ -21,8 +21,10 @@ if __name__ == '__main__':
     first_score = input("Enter your first score: ")
     second_score = input("Enter your second score: ")
     third_score = input("Enter your third score: ")
+    fourth_score = input("Enter your fourth score: ")
     try:
-        average_scores = average(int(first_score), int(second_score), int(third_score))
+        average_scores = average(int(first_score), int(second_score),
+                                 int(third_score), int(fourth_score))
     except ValueError:
         print("Please enter only positive scores!")
     else:
